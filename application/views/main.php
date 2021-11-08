c814a3b52a0b3d19f51081ee36c4199acfd3628d
Irfan <irfan@sawala.co>
2021-11-08 08:31:05 +0700

    fix image

--- application/views/main.php
+++ application/views/main.php
@@ -5,6 +5,8 @@
         </div>
         <div class="col-12 text-center mainFrame">
             <img src="<?=base_url()?>resources/img/main-1.jpg" id="mainImg" alt="" style="width: 70%;">
+            <img src="<?=base_url()?>resources/img/main-2.jpg" class="d-none" alt="" style="width: 70%;">
+            <img src="<?=base_url()?>resources/img/main-reset.jpg" class="d-none" alt="" style="width: 70%;">
         </div>
     </div>
 </div>
