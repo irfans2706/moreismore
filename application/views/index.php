9463b898a67047c85e74700efabef9efb5c967d5
Irfan <irfan@sawala.co>
2021-11-25 11:21:44 +0700

    fox: typo

--- application/views/index.php
+++ application/views/index.php
@@ -2,7 +2,7 @@
     <div class="row justify-content-center">
         <div class="col-10 col-md-6 col-lg-6 text-center" style="margin-top: 40vh;">
             <form action="<?=base_url()?>post_form" method="post">
-                <p style="font-size: 20px;" class="text-white">Bolehkah meminta perhantianmu?</p>
+                <p style="font-size: 20px;" class="text-white">Bolehkah meminta perhatianmu?</p>
                 <input type="text" name="name" class="text-white" placeholder="isi nama pemain" style="border-radius: 15px;background: transparent;text-align: center;font-size: 18px;border: 1px solid white;padding: 5px;width: 100%;display: block;" maxlength="15">
                 <button class="btn text-white" type="submit" style="margin-top: 30vh; background: transparent;">Klik untuk memulai</button>
             </form>
